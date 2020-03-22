72afcf29991a4373cf56ba8b6f1b43bb311c0b4e
ahmedpro <ahmedmuhammedpro@gmail.com>
2020-03-22 12:02:32 +0200

    Initial Commit

---
+++ RegisterAndLoginWithMap/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RegisterAndLoginWithMap
+//
+//  Created by ahmedpro on 3/22/20.
+//  Copyright © 2020 ahmedpro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ RegisterAndLoginWithMap/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  RegisterAndLoginWithMap
+//
+//  Created by ahmedpro on 3/22/20.
+//  Copyright © 2020 ahmedpro. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
